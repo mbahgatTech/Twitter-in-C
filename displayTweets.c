cdaf6989850ca1748dd8a469d367da56962cf5d0
Mazen Bahgat <mbahgat@uoguelph.ca>
2021-03-22 02:06:01 +0000

    Delete displayTweets.c

--- displayTweets.c
+++
@@ -1,9 +0,0 @@
-#include "headerA3.h"
-
-void displayTweets(tweet * tweetList) {
-    tweet *tempNode = tweetList;
-    while (tempNode != NULL) {
-        printf("%d: Created by %s: %s\n", tempNode -> id, tempNode -> user, tempNode -> text);
-        tempNode = tempNode -> next;
-    }
-}
